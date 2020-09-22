9dc2c24f01fdbe8d9b91b6f9875d658286678f6f
João <joao_s.ramos@hotmail.com>
2020-09-22 12:47:32 -0300

    acrescentando darkmode

--- docs/script.js
+++ docs/script.js
@@ -1,2 +1,25 @@
-const html = document.querySelector("html")
-const checkbox = document.querySelector("input[name=theme]")
+const themes = {
+    light: {
+      background: 'white',
+      text: 'black',
+    },
+    dark: {
+      background: 'black',
+      text: 'white',
+    }
+  };
+
+  function setTheme(newTheme) {
+    const themeColors = themes[newTheme]; // Seleciona o tema para aplicar
+  
+    Object.keys(themeColors).map(function(key) {
+      html.style.setProperty(`--${key}`, themeColors[key]); // Altera as variáveis no css
+    });
+  }
+
+  <input name="theme" type="checkbox" class="toggle-dark-mode" />
+
+  const darkModeToggle = document.querySelector('input[name=theme]');
+darkModeToggle.addEventListener('change', function({ target }) {
+  setTheme(target.checked ? 'dark' : 'light');
+});
